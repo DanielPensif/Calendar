84118d57e51790d8be1d8f83ec1ca13046842c70
MrSanechka <alex.skalin.1102@gmail.com>
2025-06-23 21:14:35 +0300

    Второй этап работы над настройками: добавленно три активности по категориям

--- app/src/main/java/com/example/Kalendar/SettingsActivity.java
+++ app/src/main/java/com/example/Kalendar/SettingsActivity.java
@@ -1,12 +1,52 @@
 package com.example.Kalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private TextView buttonNotifications;
+    private TextView buttonStyle;
+    private TextView buttonProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings); 
+        setContentView(R.layout.activity_settings);
+
+        buttonNotifications = findViewById(R.id.notificationsButton);
+        buttonStyle = findViewById(R.id.styleButton);
+        buttonProfile = findViewById(R.id.profileButton);
+
+        buttonNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, SettingsNotificationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonStyle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, SettingsStyleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, SettingsProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/Kalendar/SettingsProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.Kalendar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_profile);
+    }
+}
